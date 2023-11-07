11cb2c77a27640107f516b332a16872523792efb
Hossain <arafat.kd99@gmail.com>
2023-11-07 19:30:38 +0100

    Create routes to display components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Rockets from './Components/Rockets';
+import Missions from './Components/Missions';
 import './App.css';
+import Nav from './Components/Nav';
+import MyProfile from './Components/MyProfile';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="*" element={<div>If page not found it goes here</div>} />
+      </Routes>
+    </Router>
   );
 }
 
