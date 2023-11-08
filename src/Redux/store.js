fa981a6e6f438f2f12a168457c424baa8b4e7bff
Hossain <arafat.kd99@gmail.com>
2023-11-08 17:52:17 +0100

    Add rocket reducer to redux store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missionsSlice';
+import rocketReducer from './rocketsSlice';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rocket: rocketReducer,
   },
 });
 
