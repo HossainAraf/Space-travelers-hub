4db17571822588e7343b41240a6a881b3909f08f
Hossain <arafat.kd99@gmail.com>
2023-11-08 17:57:23 +0100

    Add async actions for rocket

--- src/Redux/rocketsSlice.js
+++ src/Redux/rocketsSlice.js
@@ -3,3 +3,10 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 // API
 const APIurl = 'https://api.spacexdata.com/v4/rockets';
+
+// ASYNC ACTIONS
+const getRockets = createAsyncThunk('rockets/getRockets', async () => {
+  const response = await fetch(APIurl);
+  const data = await response.json();
+  return data;
+});
