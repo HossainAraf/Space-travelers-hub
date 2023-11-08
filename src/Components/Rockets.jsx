b8979f3c4f923d690c67ceb93177206de8ed75a2
Hossain <arafat.kd99@gmail.com>
2023-11-08 18:14:26 +0100

    Dispatch actions for reserving and canceling rockets

--- src/Components/Rockets.jsx
+++ src/Components/Rockets.jsx
@@ -8,7 +8,42 @@ const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
 
+  
+  // /GET ROCKETS FROM LOCAL STORAGE
+  const storedRockets = JSON.parse(localStorage.getItem('rockets')) || [];
+
+  // MERGE ROCKETS DATA WITH LOCAL STORAGE
+  const mergedRockets = rockets.map((rocket) => {
+    const storedRocket = storedRockets.find((r) => r.id === rocket.id);
+    if (storedRocket) {
+      return { ...rocket, reserved: storedRocket.reserved };
+    }
+    return rocket;
+  });
+
   // Fetch rockets data on page load
   useEffect(() => {
     dispatch(getRockets());
   }, [dispatch]);
+
+  
+  // Handle reserve/cancel rocket
+  const handleReserveRocket = (id, reserved) => {
+    // Update the local storage and dispatch the action
+    if (reserved) {
+      localStorage.setItem(
+        'rockets',
+        JSON.stringify(mergedRockets.map((rocket) => (
+          rocket.id === id ? { ...rocket, reserved: false } : rocket))),
+      );
+      dispatch(cancelRocket(id));
+    } else {
+      localStorage.setItem(
+        'rockets',
+        JSON.stringify(mergedRockets.map((rocket) => (
+          rocket.id === id ? { ...rocket, reserved: true } : rocket))),
+      );
+      dispatch(reserveRocket(id));
+    }
+  };
+
