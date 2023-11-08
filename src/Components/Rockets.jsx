09d216bd2572add1806e33d7d792425ef8d508ca
Hossain <arafat.kd99@gmail.com>
2023-11-08 18:17:44 +0100

    Hnadle error and render rocket data

--- src/Components/Rockets.jsx
+++ src/Components/Rockets.jsx
@@ -1,14 +1,16 @@
 // IMPORTS
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { getRockets, reserveRocket, cancelRocket } from '../redux/rocketsSlice';
-
+import {
+  Button, Container, Row, Col,
+} from 'react-bootstrap';
+import { getRockets, reserveRocket, cancelRocket } from '../Redux/rocketsSlice';
+import '../styles/rockets.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
 
-  
   // /GET ROCKETS FROM LOCAL STORAGE
   const storedRockets = JSON.parse(localStorage.getItem('rockets')) || [];
 
@@ -26,7 +28,6 @@ const Rockets = () => {
     dispatch(getRockets());
   }, [dispatch]);
 
-  
   // Handle reserve/cancel rocket
   const handleReserveRocket = (id, reserved) => {
     // Update the local storage and dispatch the action
@@ -47,3 +48,42 @@ const Rockets = () => {
     }
   };
 
+  // Show loading, error, or rockets data
+  const renderRockets = () => {
+    if (mergedRockets.length === 0) {
+      return <p>Loading rockets...</p>;
+    }
+
+    return mergedRockets.map((rocket) => (
+      <Container className="container-rocket" key={rocket.id}>
+        <Row>
+          <div className="img-rocket">
+            <img src={rocket.image} alt={rocket.image} />
+          </div>
+          <Col>
+            <h2>{rocket.name}</h2>
+            <Row className="reserved">
+              <div className="res">
+                {rocket.reserved ? (
+                  <h5>Reserved</h5>
+                ) : null}
+              </div>
+              <p>{rocket.description}</p>
+            </Row>
+            <Button variant="primary" type="button" onClick={() => handleReserveRocket(rocket.id, rocket.reserved)}>
+              {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    ));
+  };
+
+  return (
+    <section>
+      {renderRockets()}
+    </section>
+  );
+};
+
+export default Rockets;
