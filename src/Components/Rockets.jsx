f938060c16757ff32c02e5dcfe09a84444933ad3
Hossain <arafat.kd99@gmail.com>
2023-11-08 18:11:38 +0100

    Add hooks to fetch rockets

--- src/Components/Rockets.jsx
+++ src/Components/Rockets.jsx
@@ -3,8 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getRockets, reserveRocket, cancelRocket } from '../redux/rocketsSlice';
 
-export default function Rockets() {
-  return (
-    <h2>Hello Rockets</h2>
-  );
-}
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+
+  // Fetch rockets data on page load
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
