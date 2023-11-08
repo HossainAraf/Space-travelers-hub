97dc4c88ce9ff048c99998b642616fe30935b21d
Hossain <arafat.kd99@gmail.com>
2023-11-08 17:59:13 +0100

    Create rocket reducer

--- src/Redux/rocketsSlice.js
+++ src/Redux/rocketsSlice.js
@@ -10,3 +10,48 @@ const getRockets = createAsyncThunk('rockets/getRockets', async () => {
   const data = await response.json();
   return data;
 });
+
+
+// REDUCER
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState: {
+    rockets: [],
+    status: null,
+    hasErrors: false,
+  },
+  reducers: {
+    reserveRocket: (state, action) => {
+      const updatedRockets = state.rockets.map((rocket) => (
+        rocket.id === action.payload ? { ...rocket, reserved: true } : rocket));
+      state.rockets = updatedRockets;
+    },
+    cancelRocket: (state, action) => {
+      const updatedRockets = state.rockets.map((rocket) => (
+        rocket.id === action.payload ? { ...rocket, reserved: false } : rocket));
+      state.rockets = updatedRockets;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRockets.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getRockets.fulfilled, (state, action) => {
+        state.rockets = action.payload.map((rocket) => ({
+          id: rocket.id,
+          name: rocket.name,
+          description: rocket.description,
+          image: rocket.flickr_images[0],
+        }));
+
+        state.status = 'success';
+        state.hasErrors = false;
+      })
+      .addCase(getRockets.rejected, (state) => {
+        state.status = 'failed';
+        state.hasErrors = true;
+      });
+  },
+});
+
