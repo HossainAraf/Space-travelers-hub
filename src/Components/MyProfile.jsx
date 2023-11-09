ae525974e9cf5cb012a8b0b6c261d20c50c94218
George <georgie.george7@gmail.com>
2023-11-09 21:48:11 +0400

    Add joined missions list to MyProfile component with styling

--- src/Components/MyProfile.jsx
+++ src/Components/MyProfile.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import '../styling/myprofile.css';
 
-export default function MyProfile() {
+function MyProfile() {
+  const missions = useSelector((state) => state.missions.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+
   return (
-    <h2>Hello Profile</h2>
+    <div className="summary">
+      <div className="joined-missions-rockets">
+        <h3>My Missions</h3>
+        {joinedMissions.length > 0 ? (
+          <ul className="sortedlist">
+            {joinedMissions.map((mission) => (
+              <li key={mission.mission_id}>{mission.mission_name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No missions joined yet.</p>
+        )}
+      </div>
+    </div>
   );
 }
+
+export default MyProfile;
